a9708727dc709803204e876fbf1947ef2f2bea5b
Yuohan Lee <derq3278@gmail.com>
2020-09-22 15:26:59 -0700

    Fix errors

--- models.py
+++ models.py
@@ -25,23 +25,25 @@ class Generator(nn.Module):
     """ ResNet-based Generator
     """
     def __init__(self, in_channels, out_channels, conv_dim):
-
         super(Generator, self).__init__()
 
         n_blocks = 6
-
-        self.model = nn.Sequential(
+        
+        model = [
             nn.ReflectionPad2d(3),
             nn.Conv2d(in_channels, conv_dim, kernel_size=7, bias=False),
             nn.BatchNorm2d(conv_dim),
             nn.ReLU(True),
-            nn.Conv2d(conv_dim, 2*conv_dim, kernel_size=7, bias=False),
+            nn.Conv2d(conv_dim, 2*conv_dim, kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(2*conv_dim),
             nn.ReLU(True),
-            nn.Conv2d(2*conv_dim, 4*conv_dim, kernel_size=7, bias=False),
+            nn.Conv2d(2*conv_dim, 4*conv_dim, kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(4*conv_dim),
             nn.ReLU(True),
-            nn.ModuleList([ResidualBlock(4*conv_dim) for _ in range(n_blocks)]),
+        ]
+        for _ in range(n_blocks):
+            model += [ResidualBlock(4*conv_dim)]
+        model += [
             nn.ConvTranspose2d(4*conv_dim, 2*conv_dim, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False),
             nn.BatchNorm2d(2*conv_dim),
             nn.ReLU(True),
@@ -51,7 +53,8 @@ class Generator(nn.Module):
             nn.ReflectionPad2d(3),
             nn.Conv2d(conv_dim, out_channels, kernel_size=7),
             nn.Tanh()
-        )
+        ]
+        self.model = nn.Sequential(*model)
 
     def forward(self, x):
         return self.model(x)
@@ -59,12 +62,11 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     """ PatchGAN Discriminator
     """
-    def __init__(self, in_channels, out_channels, conv_dim):
-
+    def __init__(self, in_channels, conv_dim):
         super(Discriminator, self).__init__()
         
         self.model = nn.Sequential(
-            nn.Conv2d(input_nc, conv_dim, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(in_channels, conv_dim, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(conv_dim, 2*conv_dim, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(2*conv_dim),
@@ -72,10 +74,10 @@ class Discriminator(nn.Module):
             nn.Conv2d(2*conv_dim, 4*conv_dim, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(4*conv_dim),
             nn.LeakyReLU(0.2, True),
-            nn.Conv2d(2*conv_dim, 4*conv_dim, kernel_size=4, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(4*conv_dim),
+            nn.Conv2d(4*conv_dim, 8*conv_dim, kernel_size=4, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(8*conv_dim),
             nn.LeakyReLU(0.2, True),
-            Conv2d(conv_dim*8, 1, kernel_size=4, stride=1, paddding=1)
+            nn.Conv2d(conv_dim*8, 1, kernel_size=4, stride=1, padding=1)
         )
 
     def forward(self, x):
