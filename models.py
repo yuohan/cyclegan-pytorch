01569b55a1cc54a2692b2926c5ae4fc24ac603ac
Yuohan Lee <derq3278@gmail.com>
2020-09-26 20:23:30 -0700

    Define Cycle GAN class

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+
+import itertools
 
 class ResidualBlock(nn.Module):
     """ Residual block
@@ -82,3 +85,89 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+class GeneratorLoss(nn.Module):
+    """ Generator Loss log(D(G(x)))
+    """
+    def __init__(self):
+        super(GeneratorLoss, self).__init__()
+
+        self.loss = nn.BCEWithLogitsLoss()
+
+    def forward(self, pred_fake):
+        return self.loss(pred_fake, torch.ones_like(pred_fake))
+
+class DiscriminatorLoss(nn.Module):
+    """ Discriminator Loss log(D(y) + log(1-D(G(x)))
+    """
+    def __init__(self):
+        super(DiscriminatorLoss, self).__init__()
+
+        self.loss = nn.BCEWithLogitsLoss()
+
+    def forward(self, pred_real, pred_fake):
+        return (self.loss(pred_real, torch.ones_like(pred_real)) + self.loss(pred_fake, torch.zeros_like(pred_fake))) * 0.5
+
+class CycleGAN(nn.Module):
+    """ Cycle GAN 
+    """
+    def __init__(self, in_channels, out_channels, conv_dim=64, lr=2e-4, cycle_weight=10):
+        super(CycleGAN, self).__init__()
+
+        self.G_X = Generator(in_channels, out_channels, conv_dim)
+        self.G_Y = Generator(in_channels, out_channels, conv_dim)
+
+        self.D_X = Discriminator(in_channels, conv_dim)
+        self.D_Y = Discriminator(in_channels, conv_dim)
+
+        self.criterion_G = GeneratorLoss()
+        self.criterion_D = DiscriminatorLoss()
+        self.criterion_C = nn.L1Loss()
+
+        self.optim_G = optim.Adam(itertools.chain(self.G_X.parameters(), self.G_Y.parameters()), lr, beta=[0.5, 0.999])
+        self.optim_Y = optim.Adam(itertools.chain(self.D_X.parameters(), self.D_Y.parameters()), lr, beta=[0.5, 0.999])
+        
+        self.cycle_weight = cycle_weight
+
+    def train(self, real_X, real_Y):
+
+        fake_X = self.G_Y(real_Y) # G_Y(Y)
+        fake_Y = self.G_X(real_X) # G_X(X)
+
+        cycle_X = self.G_Y(fake_Y) # G_Y(G_X(X))
+        cycle_Y = self.G_X(fake_X) # G_X(G_Y(Y))
+
+        # Loss = L_gen(G, D_Y, X, Y) + L_gen(F, D_X, Y, X) + lambda * L_cyc(G, F)
+        # L_gen(G, D, X, Y) = E[logD(y)] + E[1-logD(G(x))]
+
+        # Train the generator
+        # Disable gradient of Discriminators
+        for model in [self.D_X, self.D_Y]:
+            for param in model.parameters():
+                param.requires_grad = False
+        self.optim_G.zero_grad()
+
+        # maximize log(D(G(x))) instead of log(1-D(G(x)))
+        G_X_loss = self.criterion_G(self.D_X(fake_Y))
+        G_Y_loss = self.criterion_G(self.D_Y(fake_X))
+        # Cycle loss ||G_B(G_A(A)) - A|| + ||G_A(G_B(B)) - B||
+        cycle_loss = self.criterion_C(cycle_X, real_X) + self.criterion_C(cycle_Y, real_Y)
+        G_loss = G_X_loss + G_Y_loss + self.cycle_weight*cycle_loss
+        G_loss.backward()
+        self.optim_G.step()
+
+        # Train the discriminator
+        for model in [self.D_X, self.D_Y]:
+            for param in model.parameters():
+                param.requires_grad = True
+        self.optim_D.zero_grad()
+        
+        # pool from fake_X, fake_Y
+        D_X_loss = self.criterion_D(self.D_X(real_Y), self.D_X(fake_Y.detach()))
+        D_Y_loss = self.criterion_D(self.D_Y(real_X), self.D_Y(fake_X.detach()))
+
+        D_X_loss.backward()
+        D_Y_loss.backward()
+        self.optim_D.step()
+
+        return G_loss, (D_X_loss + D_Y_loss)
